2d715227919337a022d9a43ca330af9ac36c13f0
shreevyas <shreevyas0211@gmail.com>
2022-02-13 21:04:33 +0530

    Commit PrimeNumber

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("---Logical Programs---");
 //LogicalPrograms.Fibonacci.Fibo();
-LogicalPrograms.Perfectnumber.Perfectnum();
+//LogicalPrograms.Perfectnumber.Perfectnum();
+LogicalPrograms.PrimeNumber.Prime_num();
