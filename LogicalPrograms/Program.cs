0e1dd6e6aa2b108b721219ce8feb2040693d5ee1
shreevyas <shreevyas0211@gmail.com>
2022-02-13 20:28:55 +0530

    Commit PerfectNumber

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("---Logical Programs---");
-LogicalPrograms.Fibonacci.Fibo();
+//LogicalPrograms.Fibonacci.Fibo();
+LogicalPrograms.Perfectnumber.Perfectnum();
